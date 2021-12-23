8a5749a1f6d433c73b16756e1bc7a2319b3e3ffe
dmrock <equi.denis@gmail.com>
2021-12-23 18:26:52 +0300

    working with inputs examples added

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -17,3 +17,15 @@ test('Clicking on elements', async ({ page }) => {
 
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+
+test('Working with inputs', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.click('#signin_button')
+  await page.type('#user_login', 'wrong_username')
+  await page.type('#user_password', 'password')
+  await page.click('text=Sign in')
+
+  const errorMessage = await page.locator('.alert-error')
+
+  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
