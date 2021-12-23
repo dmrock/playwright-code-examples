8c5561e1826b80c93d4d89bc1f57e8a50d332af5
dmrock <equi.denis@gmail.com>
2021-12-23 17:47:19 +0300

    clicking examples added

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,6 +2,18 @@ import { test, expect } from '@playwright/test'
 
 test('Simple basic test', async ({ page }) => {
   await page.goto('https://example.com')
+
   const pageTitle = await page.locator('h1')
+
   await expect(pageTitle).toContainText('Example Domain')
+})
+
+test('Clicking on elements', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.click('#signin_button')
+  await page.click('text=Sign in')
+
+  const errorMessage = await page.locator('.alert-error')
+
+  await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
