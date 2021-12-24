122951516c39ad5ef6e66c74f4de417195d7ebc6
dmrock <equi.denis@gmail.com>
2021-12-24 20:29:16 +0300

    assertion examples added

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -29,3 +29,20 @@ test('Working with inputs', async ({ page }) => {
 
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+
+test.only('Asseetions', async ({ page }) => {
+  await page.goto('https://example.com')
+
+  await expect(page).toHaveURL('https://example.com')
+  await expect(page).toHaveTitle('Example Domain')
+
+  const element = await page.locator('h1')
+
+  await expect(element).toBeVisible()
+  await expect(element).toHaveText('Example Domain')
+  await expect(element).toHaveCount(1)
+
+  const nonExistingElement = await page.locator('h5')
+
+  await expect(nonExistingElement).not.toBeVisible()
+})
