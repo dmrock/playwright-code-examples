d9d822f982b686a7bf1c832457cfbd3d5c561c1c
dmrock <equi.denis@gmail.com>
2021-12-25 18:21:28 +0300

    config file added

---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { PlaywrightTestConfig } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  timeout: 60000,
+  retries: 0,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 15000,
+    ignoreHTTPSErrors: true,
+    video: 'off',
+    screenshot: 'off',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'Webkit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+}
+
+export default config
+
+// Examples:
+// npx playwright test --config=playwright.config.ts --project=Webkit
+// npx playwright test --project=Webkit
