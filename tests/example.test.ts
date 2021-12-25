94ccbb2d69b6b6d2a35e2aa552a3ca1ab681a2be
dmrock <equi.denis@gmail.com>
2021-12-25 17:07:59 +0300

    test annotations

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('Simple basic test', async ({ page }) => {
+test.skip('Simple basic test', async ({ page }) => {
   await page.goto('https://example.com')
 
   const pageTitle = await page.locator('h1')
@@ -18,31 +18,33 @@ test('Clicking on elements', async ({ page }) => {
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
 
-test('Working with inputs', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/index.html')
-  await page.click('#signin_button')
-  await page.type('#user_login', 'wrong_username')
-  await page.type('#user_password', 'password')
-  await page.click('text=Sign in')
+test.describe('Some test suite', () => {
+  test('Working with inputs', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.type('#user_login', 'wrong_username')
+    await page.type('#user_password', 'password')
+    await page.click('text=Sign in')
 
-  const errorMessage = await page.locator('.alert-error')
+    const errorMessage = await page.locator('.alert-error')
 
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
-})
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  })
 
-test.only('Asseetions', async ({ page }) => {
-  await page.goto('https://example.com')
+  test('Asseetions', async ({ page }) => {
+    await page.goto('https://example.com')
 
-  await expect(page).toHaveURL('https://example.com')
-  await expect(page).toHaveTitle('Example Domain')
+    await expect(page).toHaveURL('https://example.com')
+    await expect(page).toHaveTitle('Example Domain')
 
-  const element = await page.locator('h1')
+    const element = await page.locator('h1')
 
-  await expect(element).toBeVisible()
-  await expect(element).toHaveText('Example Domain')
-  await expect(element).toHaveCount(1)
+    await expect(element).toBeVisible()
+    await expect(element).toHaveText('Example Domain')
+    await expect(element).toHaveCount(1)
 
-  const nonExistingElement = await page.locator('h5')
+    const nonExistingElement = await page.locator('h5')
 
-  await expect(nonExistingElement).not.toBeVisible()
+    await expect(nonExistingElement).not.toBeVisible()
+  })
 })
