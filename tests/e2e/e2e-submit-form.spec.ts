cc3f971a14f908051bc5db990f37bf0abff228a2
dmrock <equi.denis@gmail.com>
2021-12-30 21:30:26 +0300

    reset form test added

---
+++ tests/e2e/e2e-submit-form.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Feedback form', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#feedback')
+  })
+  // Reset feedback form
+  test('Reset feedback form', async ({ page }) => {
+    await page.type('#name', 'Ivan')
+    await page.type('#email', 'test@mail.com')
+    await page.type('#subject', 'Isue')
+    await page.type('#comment', 'Some comment')
+    await page.click('input[name=clear]')
+
+    const nameInput = page.locator('#name')
+    const commentInput = page.locator('#comment')
+
+    await expect(nameInput).toBeEmpty()
+    await expect(commentInput).toBeEmpty()
+  })
+  // Submit reset form
+})
