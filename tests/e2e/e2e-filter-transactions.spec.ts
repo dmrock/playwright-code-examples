f87e6bc72799d9c918c8890722b62733e883be1c
Denis Sitnikov <equi.denis@gmail.com>
2022-01-02 20:06:07 +0300

    payment test added

--- tests/e2e/e2e-filter-transactions.spec.ts
+++ tests/e2e/e2e-filter-transactions.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe.only('Filter transactions', () => {
+test.describe('Filter transactions', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://zero.webappsecurity.com/')
     await page.click('#signin_button')
---
+++ tests/e2e/e2e-payment.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test'
+
+test.describe.only('New payment', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/')
+    await page.click('#signin_button')
+    await page.type('#user_login', 'username')
+    await page.type('#user_password', 'password')
+    await page.click('text=Sign in')
+  })
+  test('Should send new payment', async ({ page }) => {
+    await page.click('#pay_bills_tab')
+    await page.selectOption('#sp_payee', 'apple')
+    await page.click('#sp_get_payee_details')
+    await page.waitForSelector('#sp_payee_details')
+    await page.selectOption('#sp_account', '6')
+    await page.type('#sp_amount', '5000')
+    await page.type('#sp_date', '2022-01-02')
+    await page.type('#sp_description', 'Some description')
+    await page.click('#pay_saved_payees')
+
+    const message = page.locator('#alert_content > span')
+    await expect(message).toBeVisible()
+    await expect(message).toContainText(
+      'The payment was successfully submitted'
+    )
+  })
+})
