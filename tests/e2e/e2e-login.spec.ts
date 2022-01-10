46aaafd8f162d60c1e4926fd5cd289b6c974bde1
Denis Sitnikov <equi.denis@gmail.com>
2022-01-10 16:24:46 +0300

    create page model class for home page

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,25 +1,28 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../../page-objects/LoginPage'
+import { HomePage } from '../../page-objects/HomePage'
 
 test.describe.parallel.only('Login / Logout flow', () => {
   let loginPage: LoginPage
+  let homePage: HomePage
 
   // Befor hook
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page)
-    await loginPage.visit()
+    homePage = new HomePage(page)
+
+    await homePage.visit()
+    await homePage.clickOnSignIn()
   })
 
   // Negative scenario
   test('Negative scenario for login', async ({ page }) => {
-    await page.click('#signin_button')
     await loginPage.login('invalid username', 'invalid password')
     await loginPage.assertErrorMessage()
   })
 
   // Positive scenario + Logout
   test('Positive scenario for login and logout', async ({ page }) => {
-    await page.click('#signin_button')
     await loginPage.login('username', 'password')
 
     const accountSummaryTab = page.locator('#account_summary_tab')
