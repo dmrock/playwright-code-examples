08aa17ccb78d0edeac94bc0d06312f3bfe680ed9
Denis Sitnikov <equi.denis@gmail.com>
2022-01-10 19:31:36 +0300

    setup POM for feedback form

--- tests/e2e/e2e-submit-form.spec.ts
+++ tests/e2e/e2e-submit-form.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Feedback form', () => {
+test.describe.only('Feedback form', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://zero.webappsecurity.com/index.html')
     await page.click('#feedback')
