429b7fcd3558afc38989cddaa293c82f98905575
Denis Sitnikov <equi.denis@gmail.com>
2022-01-10 18:53:58 +0300

    implement page model for search test

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 import { LoginPage } from '../../page-objects/LoginPage'
 import { HomePage } from '../../page-objects/HomePage'
 
-test.describe.parallel.only('Login / Logout flow', () => {
+test.describe.parallel('Login / Logout flow', () => {
   let loginPage: LoginPage
   let homePage: HomePage
 
--- tests/e2e/e2e-search.spec.ts
+++ tests/e2e/e2e-search.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test'
+import { HomePage } from '../../page-objects/HomePage'
 
 test.describe('Search results', () => {
   test('Should find search results', async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/index.html')
-    await page.type('#searchTerm', 'bank')
-    await page.keyboard.press('Enter')
+    let homePage: HomePage = new HomePage(page)
+    await homePage.visit()
+    await homePage.searchFor('bank')
 
     const numberOfLinks = page.locator('li > a')
-
     await expect(numberOfLinks).toHaveCount(2)
   })
 })
