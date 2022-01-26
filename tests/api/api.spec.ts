dd5ea0093a15a62ca2b96121441b3cde4dee69c6
Denis Sitnikov <equi.denis@gmail.com>
2022-01-26 22:56:13 +0300

    Parse response JSON data example added

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -3,9 +3,12 @@ import { test, expect } from '@playwright/test'
 test.describe.parallel('API testing', () => {
   const baseUrl = 'https://reqres.in/api'
 
-  test('Simple API test - assert response status', async ({ request }) => {
+  test.only('Simple API test - assert response status', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/2`)
     expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+    console.log(responseBody)
   })
 
   test('Simple API test - assert invalid endpoint', async ({ request }) => {
