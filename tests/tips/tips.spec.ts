e89915598848426c341341d4023786110f066a7a
Denis Sitnikov <equi.denis@gmail.com>
2022-02-05 23:41:46 +0300

    Skip browser annotation

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test'
 
-test.describe.only('Tips & tricks section', () => {
+test.describe('Tips & tricks section', () => {
   test('TestInfo object', async ({ page }, testInfo) => {
     await page.goto('https://www.example.com')
     console.log(testInfo.status)
+  })
+
+  test.only('Skip browser', async ({ page, browserName }) => {
+    test.skip(browserName === 'chromium', 'Feature not implemented')
+    await page.goto('https://www.example.com')
   })
 })
