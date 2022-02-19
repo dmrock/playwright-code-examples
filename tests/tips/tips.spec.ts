88643d03943d5319e060854f89315ff926fc52f0
Denis Sitnikov <equi.denis@gmail.com>
2022-02-19 19:07:52 +0300

    Fixme annotation example

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -3,11 +3,16 @@ import { test, expect } from '@playwright/test'
 test.describe('Tips & tricks section', () => {
   test('TestInfo object', async ({ page }, testInfo) => {
     await page.goto('https://www.example.com')
-    console.log(testInfo.status)
+    // console.log(testInfo.status)
   })
 
   test('Skip browser', async ({ page, browserName }) => {
     test.skip(browserName === 'chromium', 'Feature not implemented')
+    await page.goto('https://www.example.com')
+  })
+
+  test('Fixme annotation', async ({ page, browserName }) => {
+    test.fixme(browserName === 'chromium', 'Test is not stable, needs revision')
     await page.goto('https://www.example.com')
   })
 })
